551c59ff53d5e14e016582b3b06b92b36f742117
nkinkade <nkinkade@committer.creativecommons.org>
2008-03-24 23:33:07 +0000

    Merged revisions 9488 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/bbpress/trunk
    ........
      r9488 | nkinkade | 2008-03-24 16:32:44 -0700 (Mon, 24 Mar 2008) | 1 line
      
      Tweaked license requirement verbiage, and removed an extraneous colon
    ........

--- bb-templates/cc/register.php
+++ bb-templates/cc/register.php
@@ -41,13 +41,13 @@ endif;
 
 <div style='margin: 2ex;'>
 <?php
-_e('<strong>IMPORTANT</strong> copyright information: All content submitted to this forum is released under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>. By checking the following box you agree to this condition of use.  If you are not willing to release all content you submit under this license then you should not register.');
+_e('<strong>IMPORTANT</strong> copyright information: All content submitted to this forum is released under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>. By checking the following box you agree to this condition of use.  If you are not willing to release all content you submit to this forum under this license then you should not register.');
 ?>
 </div>
 <div style='margin: 2ex;'>
-    <input type="checkbox" name="terms_cond" />:
+    <input type="checkbox" name="terms_cond" /> &nbsp;
 <?php
-_e('I agree to release <span style="text-decoration: underline;">anything</span> I submit to this forum under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>.');
+_e('I agree to release <span style="text-decoration: underline;">everything</span> I submit to this forum under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>.');
 if ( $terms_cond === false ) {
     echo "<div><span style='color: red;'>" . __('You must accept the licensing terms in order to regsiter.') . "</span></div>";
 }

a7d90857bfff04ba226ddcc0fce3ea3afb6a5758
bse <bse@committer.creativecommons.org>
2009-10-23 21:47:18 +0000

    Added Google Analytics

--- bb-templates/cc/footer.php
+++ bb-templates/cc/footer.php
@@ -53,6 +53,16 @@ HTML;
 do_action('bb_foot', '');
 
 echo <<<HTML
+<!-- Google Analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-2010376-17");
+pageTracker._trackPageview();
+} catch(err) {}</script>
 </body>
 
 </html>

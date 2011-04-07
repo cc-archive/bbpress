e636c53f4753711a57596f3309021e09f9f3e427
nkinkade <nkinkade@committer.creativecommons.org>
2011-04-07 19:01:46 +0000

    Merged revisions 14991 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/bbpress/trunk
    ........
      r14991 | nkinkade | 2011-04-07 15:01:09 -0400 (Thu, 07 Apr 2011) | 1 line
      
      Added trailing slashes to a few links to avoid redirects.
    ........

--- bb-templates/cc/header.php
+++ bb-templates/cc/header.php
@@ -83,12 +83,12 @@ echo <<<HTML
 
         <li><a href="http://creativecommons.org/about">About</a></li>
         <li><a href="http://creativecommons.org/weblog">News</a></li>
-        <li><a href="https://creativecommons.net/donate">Donate</a></li>
+        <li><a href="https://creativecommons.net/donate/">Donate</a></li>
         <li><a href="http://wiki.creativecommons.org/FAQ">FAQ</a></li>
         <li><a href="http://wiki.creativecommons.org/">Wiki</a></li>
         <li><a href="http://creativecommons.org/projects">Projects</a></li>
 
-        <li><a href="https://creativecommons.net/store">Store</a></li>
+        <li><a href="https://creativecommons.net/store/">Store</a></li>
         <li class="helpLink" id="international_list"><a href="http://wiki.creativecommons.org/CC_Affiliate_Network">International</a></li>
       </ul>
     </div>
